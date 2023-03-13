edfda1ccccd117b82592b72734776dc1bfa1e46e
pc <ph.dmitry.rudenko@gmail.com>
2023-03-13 20:02:05 +0200

    fix import axios

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
-const axios = require('axios')
+import axios from 'axios'
 
-const CURRENCY_EXCHANGE_DATA_URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
+const CURRENCY_EXCHANGE_DATA_URL =
+	'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
 const DEFAULT_CURRENCY = 'USD'
 const UAH_CURRENCY = {
-  txt: 'Українська гривня',
-  rate: 1,
-  cc: 'UAH',
+	txt: 'Українська гривня',
+	rate: 1,
+	cc: 'UAH',
 }
 
 const foundCurrency = (currency) => (item) => item.cc === currency
 
 const getCurrencyExchangeData = async () => {
-  const { data } = await axios.get(CURRENCY_EXCHANGE_DATA_URL)
-  return data
+	const { data } = await axios.get(CURRENCY_EXCHANGE_DATA_URL)
+	return data
 }
 
-const exchangeCurrency = async (amount, fromCurrency, toCurrency = DEFAULT_CURRENCY) => {
-  if (!amount || !fromCurrency || !toCurrency) {
-    return 0
-  }
+const exchangeCurrency = async (
+	amount,
+	fromCurrency,
+	toCurrency = DEFAULT_CURRENCY
+) => {
+	if (!amount || !fromCurrency || !toCurrency) {
+		return 0
+	}
 
-  if (fromCurrency === toCurrency) {
-    return amount
-  }
+	if (fromCurrency === toCurrency) {
+		return amount
+	}
 
-  const currencyExchangeData = await getCurrencyExchangeData()
+	const currencyExchangeData = await getCurrencyExchangeData()
 
-  currencyExchangeData.push(UAH_CURRENCY)
+	currencyExchangeData.push(UAH_CURRENCY)
 
-  const fromCurrencyData = currencyExchangeData.find(
-    foundCurrency(fromCurrency)
-  )
-  const toCurrencyData = currencyExchangeData.find(
-    foundCurrency(toCurrency)
-  )
+	const fromCurrencyData = currencyExchangeData.find(
+		foundCurrency(fromCurrency)
+	)
+	const toCurrencyData = currencyExchangeData.find(foundCurrency(toCurrency))
 
-  if (!fromCurrencyData || !toCurrencyData) {
-    return 0
-  }
+	if (!fromCurrencyData || !toCurrencyData) {
+		return 0
+	}
 
-  const inFromCurrencySum = amount * fromCurrencyData.rate
-  const inToCurrencySum = inFromCurrencySum / toCurrencyData.rate
+	const inFromCurrencySum = amount * fromCurrencyData.rate
+	const inToCurrencySum = inFromCurrencySum / toCurrencyData.rate
 
-  return +inToCurrencySum.toFixed(2)
+	return +inToCurrencySum.toFixed(2)
 }
 
 export default {
-  exchangeCurrency,
-  getCurrencyExchangeData,
+	exchangeCurrency,
+	getCurrencyExchangeData,
 }
