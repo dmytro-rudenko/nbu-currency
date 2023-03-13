35382809ccbca36811e1275374f98d2816ae4f1a
pc <ph.dmitry.rudenko@gmail.com>
2023-03-13 18:25:42 +0200

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+const axios = require('axios')
+
+const CURRENCY_EXCHANGE_DATA_URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
+const DEFAULT_CURRENCY = 'USD'
+const UAH_CURRENCY = {
+  txt: 'Українська гривня',
+  rate: 1,
+  cc: 'UAH',
+}
+
+const foundCurrency = (currency) => (item) => item.cc === currency
+
+const getCurrencyExchangeData = async () => {
+  const { data } = await axios.get(CURRENCY_EXCHANGE_DATA_URL)
+  return data
+}
+
+const exchangeCurrency = async (amount, fromCurrency, toCurrency = DEFAULT_CURRENCY) => {
+  if (!amount || !fromCurrency || !toCurrency) {
+    return 0
+  }
+
+  if (fromCurrency === toCurrency) {
+    return sum
+  }
+
+  const currencyExchangeData = await getCurrencyExchangeData()
+
+  currencyExchangeData.push(UAH_CURRENCY)
+
+  const fromCurrencyData = currencyExchangeData.find(
+    foundCurrency(fromCurrency)
+  )
+  const toCurrencyData = currencyExchangeData.find(
+    foundCurrency(toCurrency)
+  )
+
+  if (!fromCurrencyData || !toCurrencyData) {
+    return 0
+  }
+
+  const inFromCurrencySum = amount * fromCurrencyData.rate
+  const inToCurrencySum = inFromCurrencySum / toCurrencyData.rate
+
+  return +inToCurrencySum.toFixed(2)
+}
+
+export default {
+  exchangeCurrency,
+  getCurrencyExchangeData,
+}
